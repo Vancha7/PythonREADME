bbb5ad0c5c7181930b59fb10affbb9e301c24350
User <Vancha-15@mail.ru>
2026-02-26 22:31:18 +0300

    Урок #1 - Selenium. Блок #2 Python.

--- Python по блокам/Python. BLOK - #2. Selenium/#1.Selenium.py
+++
@@ -1,8 +0,0 @@
-#AQA \0/ - открытие WebDriver с URL.
-import time
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com")
-time.sleep(5)
-print(driver.title)
